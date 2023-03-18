20e22e3c5c7c3e1be8a603d51c201123d6aef384
Yi Wang <yi.wang.2005@gmail.com>
2023-03-18 16:54:57 -0700

    Make 02 and 03 echo to each other

--- 02-nsobject.m
+++ 02-nsobject.m
@@ -1,6 +1,13 @@
 // clang 02-nsobject.m -framework Foundation -o /tmp/a && /tmp/a
 #import <Foundation/Foundation.h>
 int main() {
+  // Every object in Apple/Objecitve-C is derived from NSObject, and,
+  // according to https://stackoverflow.com/a/3659218/724872, NSObject
+  // has a method autorelease.  When we create an object in the heap,
+  // the construction process calls autorelease, which inserts self
+  // into the autorelease pool.  At the end of the function main, we
+  // are supposed to call autorelease pool's drain method, which
+  // deallocate all objects registered in this pool.
   if ([[NSObject class] respondsToSelector:@selector(autorelease)]) {
     NSLog(@"Yes, NSObject has the method autorelease");
   }
